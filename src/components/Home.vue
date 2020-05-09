568ced0974da089f662a69766e27cc56ea125695
wooistxx <wooistxx@gmail.com>
2020-05-09 19:04:27 +0800

    db query and show ok

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,35 @@
   <div class="Home-page">
     <MenuBar />
     <div class="content" ref="content">
-      cccc
+      <center>
+        <h1>{{ msg }}</h1>
+      <table border="1px">
+        <thead>
+        <tr>
+            <th>消费流水</th> <!-- turnid  -->
+            <th>消费序号</th> <!-- turnno -->
+            <th>会卡属性</th> <!-- cardmode -->
+            <th>会卡类别</th> <!-- cardtype -->
+            <th>会卡编号</th> <!-- cardid -->
+            <th>会卡卡号</th> <!-- cardno -->
+            <th>会员名字</th> <!-- leaguename -->
+            <th>消费类别</th> <!-- itemtype -->
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="data in datas" v-bind:key="data.turnid">
+            <td v-text="data.turnid"></td>
+            <td v-text="data.turnno"></td>
+            <td v-text="data.cardmode"></td>
+            <td v-text="data.cardtype"></td>
+            <td v-text="data.cardid"></td>
+            <td v-text="data.cardno"></td>
+            <td v-text="data.leaguename"></td>
+            <td v-text="data.itemtype"></td>
+        </tr>
+        </tbody>
+    </table>
+    </center>
     </div>
   </div>
 </template>
@@ -11,6 +39,7 @@
 <script>
 
 import MenuBar from '@/components/MenuBar.vue'
+import axios from 'axios'
 
 const { ipcRenderer} = require('electron')
 export default {
@@ -20,6 +49,17 @@ export default {
   },
   watch:{
     MenuBar
+  },
+  data: () => ({
+    msg: "Error,db query failed",
+    datas: []
+    
+  }),
+  created: function(){
+    axios.get("queryAll").then(response => {
+      this.datas = response.data.data;
+      this.msg = "db query success!"
+    })
   },
   mounted () {
     this.changeWindowSize()
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports={
+  devServer: {
+    proxy: 'http://39.102.92.43:3000'
+  }
+}
+
+
